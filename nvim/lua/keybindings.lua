3074cbc29f4a3048c3549c7ef48154f5bf6ff244
wyntau <2631733+wyntau@users.noreply.github.com>
2023-05-12 11:05:02 +0800

    refactor: udpate keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -10,5 +10,25 @@ keymap('n', '<leader>w', ':w<CR>')
 keymap('n', '<leader>q', ':q<CR>')
 keymap('n', '<leader>r', ':w<CR>:source %<CR>')
 
+-- 设置 jk 快速进入normal模式
 keymap('i', 'jk', '<Esc>')
 keymap('i', 'kj', '<Esc>')
+
+-- set wrap之后，将自动换行的长行当做多行处理, 方便在长行之间跳转.
+keymap('n', 'k', 'gk')
+keymap('n', 'j', 'gj')
+
+-- remap U to <C-r> for easier redo
+keymap('n', 'U', '<C-r>')
+
+-- 插入模式, 使用ctrl-l跳出右括号
+keymap('i', '<C-l>', '<Right>')
+keymap('i', '<C-h>', '<Left>')
+keymap('i', '<C-j>', '<Down>')
+keymap('i', '<C-k>', '<Up>')
+keymap('i', '<C-a>', '<Esc>_i')
+keymap('i', '<C-e>', '<Esc>g_a')
+
+-- Move across wrapped lines like regular lines
+keymap('n', '0', '^') -- Go to the first non-blank character of a line
+keymap('n', '^', '0') -- Just in case you need to go to the very beginning of a line
