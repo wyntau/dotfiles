08ef50be6d364e69e25e338c92be21ed12faa737
wyntau <2631733+wyntau@users.noreply.github.com>
2024-05-23 19:25:38 +0800

    refactor: update nvim config

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -9,8 +9,8 @@ vim.opt.number = true
 -- 启用相对行号
 vim.opt.relativenumber = true
 --  插入模式下用绝对行号, 普通模式下用相对
+vim.api.nvim_create_autocmd({'FocusLost', 'InsertEnter'}, { command = ':set norelativenumber' })
 vim.api.nvim_create_autocmd({'FocusGained', 'InsertLeave'}, { command = ':set relativenumber' })
-vim.api.nvim_create_autocmd({'FocusLost', 'InsertEnter'}, { command = ':set norelativenumber number' })
 vim.opt.colorcolumn = '80,100'
 
 -- 使用空格代替制表符
