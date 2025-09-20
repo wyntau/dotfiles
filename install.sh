bb0070fc947bd6b0e3dd9ed4d1eaed2d5f61ee6b
wyntau <2631733+wyntau@users.noreply.github.com>
2025-09-20 09:33:17 +0800

    fix: fix homebrew install url

--- install.sh
+++ install.sh
@@ -560,7 +560,7 @@ function install_homebrew(){
   export HOMEBREW_CORE_GIT_REMOTE="https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
   export HOMEBREW_PIP_INDEX_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
 
-  /bin/bash -c "$(curl -fsSL https://github.com/Homebrew/install/raw/master/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/refs/heads/main/install.sh)"
 
   success "Successfully installed homebrew"
 }
